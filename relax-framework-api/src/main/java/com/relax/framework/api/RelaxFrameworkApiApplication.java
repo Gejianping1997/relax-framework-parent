e89e842ff8576764c4b46f46a3edbeecc702ad4c
Administrator <admin@example.com>
2020-05-07 14:24:00 +0800

    init

---
+++ relax-framework-api/src/main/java/com/relax/framework/api/RelaxFrameworkApiApplication.java
@@ -0,0 +1,13 @@
+package com.relax.framework.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelaxFrameworkApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RelaxFrameworkApiApplication.class, args);
+    }
+
+}
---
+++ relax-framework-api/src/test/java/com/relax/framework/api/RelaxFrameworkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.relax.framework.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelaxFrameworkApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
