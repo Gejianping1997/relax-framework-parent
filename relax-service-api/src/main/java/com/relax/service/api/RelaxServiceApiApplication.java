424b2eac9f5add19e4ebaff48140ef183f470776
gjp <gjp@winning.com.cn>
2020-05-22 16:17:00 +0800

    ManageCms初始化

--- relax-service-api/src/main/java/com/relax/service/api/RelaxServiceApiApplication.java
+++ relax-service-api/src/main/java/com/relax/service/api/RelaxServiceApiApplication.java
@@ -2,6 +2,7 @@ package com.relax.service.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * @author Gejianping
--- relax-service-api/src/main/java/com/relax/service/api/cms/CmsPageControllerApi.java
+++ relax-service-api/src/main/java/com/relax/service/api/cms/CmsPageControllerApi.java
@@ -2,12 +2,17 @@ package com.relax.service.api.cms;
 
 import com.relax.framework.domain.cms.request.QueryPageRequest;
 import com.relax.framework.model.response.QueryResponseResult;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 
 /**
  * @author Gejianping
  * @version 0.0.1
  * @date 2020/05/22 14:27:
  */
+@Api(value="CMS页面管理接口",description = "CMS页面管理接口，提供页面的增、删、改、查")
 public interface CmsPageControllerApi {
     /** 查询所有CmsPage列表的数据源
      * @param pageNum 请求第几页的数据
