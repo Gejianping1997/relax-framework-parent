a2fa7ec16ec0fb78ed9b2f236fcdf9b223a204c4
gjp <gjp@winning.com.cn>
2020-06-03 10:08:19 +0800

    更改MongoDB数据库实体类的包名称

--- relax-service-manage-cms/src/main/java/com/relax/service/managecms/RelaxServiceManageCmsApplication.java
+++ relax-service-manage-cms/src/main/java/com/relax/service/managecms/RelaxServiceManageCmsApplication.java
@@ -3,7 +3,10 @@ package com.relax.service.managecms;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.client.OkHttp3ClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
@@ -23,4 +26,8 @@ public class RelaxServiceManageCmsApplication {
         SpringApplication.run(RelaxServiceManageCmsApplication.class, args);
     }
 
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate(new OkHttp3ClientHttpRequestFactory());
+    }
 }
